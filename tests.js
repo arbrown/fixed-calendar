7fbb139ed681c15127543c316c7739148bfa440d
Andrew Brown <andrewrbrown1@gmail.com>
2015-10-14 16:50:03 -0600

    Added first source file and unit test stub.

--- tests.js
+++ tests.js
@@ -1,3 +1,10 @@
 QUnit.test("Hello, test", function(assert) {
+
   assert.ok(1=="1", "passed!");
+});
+
+
+QUnit.test("toFixedDate(d) exists", function(assert) {
+  var fixed = toFixedDate(new Date());
+  assert.ok(fixed != null);
 });
