825862e2f0dd0552b738b41a575c87a8259426ef
Andrew Brown <andrewrbrown1@gmail.com>
2015-10-15 00:17:47 -0600

    Added fixed calendar functionality and unit tests.

--- fixedCalendar.js
+++ fixedCalendar.js
@@ -1,3 +1,45 @@
-function toFixedDate(date){
-  return {};
+Date.prototype.getDayOfYear = function() {
+  var dayMs = 24 * 60 * 60 * 1000;
+  var janFirst = new Date(this.getFullYear(),0,1);
+  // 'date' is date at 2 am to ensure that daylight saving doesn't
+  // screw up the calculation of ms since year start.
+  var date = new Date(this.getFullYear(), this.getMonth(), this.getDate(), 2)
+  return Math.ceil((date-janFirst) / dayMs);
+};
+
+function isLeapYear(year){
+  return year%4==0&&(year%100!=0||year%400==0);
+}
+
+Date.prototype.toFixedDate = function(){
+  var months = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "Sol",
+    "July", "August", "September",
+    "October", "November", "December"
+  ]
+
+  var days = this.getDayOfYear();
+  var month = Math.floor((days-1)/ 28);
+  var date = days%28;
+  if (date == 0) {
+    date = 28;
+  }
+  var leapYear = isLeapYear(this.getFullYear());
+  if (leapYear){
+    if (days == 169){
+      // special leap day case
+      return {monthName: "Leap Day", date: 0, isSpecial: true}
+    }
+    else if (days > 169) {
+      date = (days-1) % 28;
+    }
+  }
+  if (days > 364){
+    return {monthName: "Year Day", date: 0, isSpecial: true}
+  }
+  else {
+    return {monthName: months[month], date: date};
+  }
 }
--- tests.js
+++ tests.js
@@ -1,10 +1,75 @@
 QUnit.test("Hello, test", function(assert) {
-
   assert.ok(1=="1", "passed!");
 });
 
 
 QUnit.test("toFixedDate(d) exists", function(assert) {
-  var fixed = toFixedDate(new Date());
-  assert.ok(fixed != null);
+  assert.ok(Date.prototype.toFixedDate != null);
+});
+
+QUnit.assert.DoY = function(t){
+
+  var date = new Date(t.year, t.month-1, t.date);
+  var actual = date.getDayOfYear();
+  QUnit.push(actual == t.result, actual, t.result, date.toString() +
+   " should be day number " + t.result);
+}
+
+QUnit.test("Day of Year Test", function(a){
+  var tests =
+    [
+      {month: 1, date: 1, year: 2015, result: 1},
+      {month: 12, date: 31, year: 2015, result: 365},
+      {month: 12, date: 31, year: 2016, result: 366},
+      {month: 10, date: 14, year: 2015, result: 287},
+      {month: 2, date: 28, year: 2015, result: 59},
+      {month: 4, date: 3, year: 2012, result: 94},
+    ];
+    tests.forEach(function(t) {
+      a.DoY(t);
+    });
+});
+
+QUnit.test("Leap Year Test", function(a){
+  a.ok(isLeapYear(2012), "2012 should be a leap year");
+  a.ok(isLeapYear(2016), "2016 should be a leap year");
+  a.notOk(isLeapYear(2015), "2015 should not be a leap year");
+  a.notOk(isLeapYear(2007), "2007 should not be a leap year");
+  a.notOk(isLeapYear(1900), "1900 should not be a leap year");
+  a.notOk(isLeapYear(2100), "2100 should not be a leap year");
+  a.ok(isLeapYear(2000), "2000 should be a leap year");
+});
+
+QUnit.assert.fixTest = function(t){
+  var fd = t.date.toFixedDate();
+  QUnit.push(fd.monthName == t.expected.monthName
+     && fd.date == t.expected.date,
+     fd, t.expected,
+     t.date.toString() + " should be " + t.expected.monthName + " "
+     + t.expected.date + " (fixed)."
+     )
+};
+
+QUnit.test("Fixed Date Conversion Tests", function(a) {
+  var tests = [
+    {date: new Date(2015, 0, 1), expected: {monthName: "January", date: 1}},
+    {date: new Date(2015, 0, 28), expected: {monthName: "January", date: 28}},
+    {date: new Date(2015, 0, 29), expected: {monthName: "February", date: 1}},
+    {date: new Date(2012, 5, 17), expected: {monthName: "Leap Day", date: 0}},
+    {date: new Date(1999, 11, 31), expected: {monthName: "Year Day", date: 0}},
+    {date: new Date(2012, 5, 18), expected: {monthName: "Sol", date: 1}},
+    {date: new Date(2014, 7, 4), expected: {monthName: "July", date: 20}},
+    {date: new Date(2012, 7, 4), expected: {monthName: "July", date: 20}},
+    {date: new Date(2014, 3, 3), expected: {monthName: "April", date: 9}},
+    {date: new Date(2012, 3, 3), expected: {monthName: "April", date: 10}},
+    {date: new Date(2015, 9, 7), expected: {monthName: "September", date: 28}},
+    {date: new Date(2012, 11, 3), expected: {monthName: "December", date: 1}},
+    {date: new Date(2012, 5, 19), expected: {monthName: "Sol", date: 2}},
+    {date: new Date(2013, 5, 19), expected: {monthName: "Sol", date: 2}},
+  ];
+
+  tests.forEach(function(t){
+    a.fixTest(t)
+  });
+
 });
