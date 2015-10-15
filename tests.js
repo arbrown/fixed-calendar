07cb47e0c6a97a4c421e35133455df503a4b0702
Andrew Brown <andrewrbrown1@gmail.com>
2015-10-15 00:20:49 -0600

    Removing sample unit test.

--- tests.js
+++ tests.js
@@ -1,8 +1,3 @@
-QUnit.test("Hello, test", function(assert) {
-  assert.ok(1=="1", "passed!");
-});
-
-
 QUnit.test("toFixedDate(d) exists", function(assert) {
   assert.ok(Date.prototype.toFixedDate != null);
 });
